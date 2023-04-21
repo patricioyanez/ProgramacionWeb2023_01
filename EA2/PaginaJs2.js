ff72b2f0fc52f91da7aaddff5090c9a4eec71519
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-21 09:53:45 -0400

    update

--- EA2/PaginaJs2.js
+++ EA2/PaginaJs2.js
@@ -2,11 +2,49 @@ function sumar()
 {
     let n1 = document.getElementById("numero1").value;
     let n2 = document.getElementById("numero2").value;
-    let total = parseInt(n1) +  parseInt(n2);
+    let total = parseInt("0" + n1) +  parseInt("0" + n2);
     document.getElementById("resultado").value = total;
+}
+function limpiar()
+{
+    document.getElementById("numero1").value = "";
+    document.getElementById("numero2").value = "";
+    document.getElementById("resultado").value = "";
 }
 
 
 // Ejercicios: Agregar botones y funciones para realizar:
 // resta, multiplicación y división (evitar dividir por cero
 // y que no esten vacios).
+function restar()
+{
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+    let total = parseInt("0" + n1) -  parseInt("0" + n2);
+    document.getElementById("resultado").value = total;
+}
+
+function multiplicar()
+{
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+    let total = parseInt("0" + n1) *  parseInt("0" + n2);
+    document.getElementById("resultado").value = total;
+    // NaN
+}
+
+function dividir()
+{
+    let n1 = document.getElementById("numero1").value;
+    let n2 = document.getElementById("numero2").value;
+    n1 = parseInt("0" + n1);
+    n2 = parseInt("0" + n2);
+
+    if(n2 == 0)
+    {
+        alert("No se puede dividir por cero");
+        return;
+    }
+    let total = n1 /  n2;
+    document.getElementById("resultado").value = total;
+}2
