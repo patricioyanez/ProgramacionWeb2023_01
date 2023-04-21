dd3fa989b4fd0ab94fca5d5e8dfc4a9f8b9776cd
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-21 09:13:52 -0400

    Update PaginaJs2.js

--- EA2/PaginaJs2.js
+++ EA2/PaginaJs2.js
@@ -5,3 +5,8 @@ function sumar()
     let total = parseInt(n1) +  parseInt(n2);
     document.getElementById("resultado").value = total;
 }
+
+
+// Ejercicios: Agregar botones y funciones para realizar:
+// resta, multiplicación y división (evitar dividir por cero
+// y que no esten vacios).
