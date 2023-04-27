a904b4e78fc84f0421a4c2e007337868330848d9
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-04-27 13:17:23 -0400

    update

--- EA2/PaginajQueryValidar.js
+++ EA2/PaginajQueryValidar.js
@@ -1,8 +1,46 @@
 $(function()
 {
     // codigo jQuery o JS
+
+    let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/
+
+    $('.btnLimpiar').click(function()
+    {
+        $('.txtRut, .txtDv, .txtNombre, .txtEmail').val('');
+        $('.txtRut').focus();
+    })
+
     let numeros = '0123456789';
     let letras  = 'qwertyuiopasdfghjklñzxcvbnmQWERTYUIOPASDFGHJKLÑZXCVBNMÁÉÍÓÚáéíóú';
+
+    $('.txtRut').keypress(function(e)
+    { 
+        let caracter = String.fromCharCode(e.which);
+        if(numeros.indexOf(caracter) < 0)
+            return false;
+    });
+
+    $('.txtDv').keypress(function(e)
+    { 
+        let patron = numeros + 'kK';
+        let caracter = String.fromCharCode(e.which);
+        if(patron.indexOf(caracter) < 0)
+            return false;
+    });
+    
+    $('.txtNombre').keypress(function(e)
+    { 
+        let caracter = String.fromCharCode(e.which);
+        if(letras.indexOf(caracter) < 0)
+            return false;
+    });
+    $('.txtEmail').keypress(function(e)
+    { 
+        let patron = numeros + letras + '-_.@';
+        let caracter = String.fromCharCode(e.which);
+        if(patron.indexOf(caracter) < 0)
+            return false;
+    });
     // validación campos vacios
     $('.btnEnviar').click(function()
     {
