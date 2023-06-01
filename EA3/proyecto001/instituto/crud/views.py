615de842418a265242051db956befa6ff78e8b98
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-06-01 16:23:04 -0400

    update

--- EA3/proyecto001/instituto/crud/views.py
+++ EA3/proyecto001/instituto/crud/views.py
@@ -3,6 +3,7 @@ from .models import Marca
 # Create your views here.
 
 def marca(request):
+    context = {}
+    context['listado'] = Marca.objects.all() # select * from marca
 
-
-    return render(request, 'marca.html')
+    return render(request, 'marca.html', context)
