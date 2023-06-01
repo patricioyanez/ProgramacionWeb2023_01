08ec8fa3a637f5cb14b022b9f37eef566f1315cc
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-06-01 12:28:38 -0400

    update

--- EA3/proyecto001/instituto/crud/admin.py
+++ EA3/proyecto001/instituto/crud/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Marca
+from .models import Marca, Categoria, Cliente,Genero
 # Register your models here.
 
 admin.site.register(Marca)
+admin.site.register(Categoria)
+admin.site.register(Cliente)
+admin.site.register(Genero)
--- EA3/proyecto001/instituto/crud/models.py
+++ EA3/proyecto001/instituto/crud/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+# PASOS:
+# 1.- preparar la migración
+# py manage.py makemigrations crud
+# 2.- ejecutar la migración
+# py manage.py migrate
+
+
+### otras clases que representan los modelos
 # Create your models here.
 class Marca(models.Model):
     idMarca= models.AutoField(primary_key=True)
@@ -9,11 +17,35 @@ class Marca(models.Model):
     def __str__(self):
         return self.nombre
     
-# PASOS:
-# 1.- preparar la migración
-# py manage.py makemigrations crud
-# 2.- ejecutar la migración
-# py manage.py migrate
+class Categoria(models.Model):
+    idCategoria= models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, null=False, unique=True)
+    activo = models.BooleanField()
 
+    def __str__(self):
+        return self.nombre
+    
 
-### otras clases que representan los modelos
+class Genero(models.Model):
+    idGenero= models.AutoField(db_column='idGenero', primary_key=True)
+    nombre = models.CharField(max_length=20, unique=True, blank=False, null=False)
+    activo = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):    
+    rut = models.IntegerField(primary_key=True, unique=True, null=False, blank=False)
+    dv = models.CharField(max_length=1, null=False, blank=False, default="")
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    direccion = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
+    telefono = models.CharField(max_length=20, null=False, blank=False)
+    genero = models.ForeignKey(Genero, db_column='idGenero', 
+                                on_delete=models.CASCADE, null=False, blank=False)
+    fechaNacimiento = models.DateField(null=False, blank=False)
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre + " " + self.apellido
